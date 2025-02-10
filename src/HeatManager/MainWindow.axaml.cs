27bffa31e7824717c0014f8b5b9653eab2b36d95
Matej Stehlo <stehlomatej@gmail.com>
2025-02-10 17:26:24 +0100

    Added avalonia project temlate and `.editorconfig` file

---
+++ src/HeatManager/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace HeatManager
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
