3f1c5379f16606da2da27ae233e5fe64ced2ab6d
semma7 <emmaj.solyom@gmail.com>
2025-05-26 13:29:42 +0200

    more progress

--- src/HeatManager/Views/Overview/WeeklyStatistics.axaml.cs
+++ src/HeatManager/Views/Overview/WeeklyStatistics.axaml.cs
@@ -1,5 +1,10 @@
+using Avalonia;
 using Avalonia.Controls;
+using System;
+using Avalonia.Reactive;
 using HeatManager.ViewModels.Overview;
+using Avalonia.VisualTree;
+using System.Linq;
 
 namespace HeatManager.Views.Overview;
 
@@ -9,5 +14,23 @@ public partial class WeeklyStatistics : UserControl
     {
         InitializeComponent();
         DataContext = new WeeklyStatisticsViewModel();
+
+        var border = this.FindControl<Border>("WSBorder");
+        if (border != null)
+        {
+            border.GetObservable(BoundsProperty).Subscribe(
+                new AnonymousObserver<Rect>(bounds =>
+                {
+                    // Calculate spacing based on height of the border
+                    double spacing = Math.Max(3, bounds.Height * 0.033);
+
+                    foreach (var tb in border.GetVisualDescendants().OfType<TextBlock>())
+                    {
+                        if (tb.Classes.Contains("normaltext") || tb.Classes.Contains("subheading"))
+                            tb.Margin = new Thickness(0, 0, 0, spacing);
+                    }
+                })
+            );
+        }
     }
 }
