29d3f2ccaf7b799dc2c02c4657f0e34d6b090a67
letitko <martin.demcak.05@gmail.com>
2025-05-29 11:36:18 +0200

    finished menu

--- src/HeatManager/ViewModels/MainWindowViewModel.cs
+++ src/HeatManager/ViewModels/MainWindowViewModel.cs
@@ -34,6 +34,20 @@ namespace HeatManager.ViewModels;
 public partial class MainWindowViewModel(IAssetManager assetManager, ISourceDataProvider dataProvider, IOptimizer optimizer, IProjectManager projectManager, IDataLoader dataLoader, Window window, IServiceProvider serviceProvider) : ViewModelBase
 {
 
+    public enum ViewType
+    {
+        Overview,
+        ConfigPanel,
+        Optimizer,
+        GridProduction,
+        DataExport,
+        ProjectManager,
+        SaveProject
+    }
+
+    [ObservableProperty]
+    private ViewType currentViewType;
+
     [ObservableProperty]
     private UserControl? currentView;
 
@@ -42,11 +56,7 @@ public partial class MainWindowViewModel(IAssetManager assetManager, ISourceData
     //     // Set the default view to OverviewView
     //     CurrentView = new OverviewView { DataContext = new OverviewViewModel(this) };
     // }
-    [ObservableProperty]
-    private bool isOverviewSelected = true;
-    public string OverviewIconPath => isOverviewSelected
-    ? "avares://HeatManager/Assets/Icons/selected-button-icon.png"
-    : "avares://HeatManager/Assets/Icons/deselected-button-icon.png";
+    
 
 
 
@@ -59,41 +69,50 @@ public partial class MainWindowViewModel(IAssetManager assetManager, ISourceData
     [RelayCommand]
     internal void SetConfigPanelView()
     {
-
+        CurrentViewType = ViewType.ConfigPanel;
         CurrentView = new AssetManagerView { DataContext = new AssetManagerViewModel(assetManager, optimizer) };
     }
 
     [RelayCommand]
     private void SetOptimizerView()
     {
+        CurrentViewType = ViewType.Optimizer;
         CurrentView = new DataOptimizerView { DataContext = new DataOptimizerViewModel(optimizer) };
     }
 
     [RelayCommand]
     private void SetGridProductionView()
     {
+        CurrentViewType = ViewType.GridProduction;
         CurrentView = new GridProductionView { DataContext = new GridProductionViewModel(dataProvider) };
     }
 
     [RelayCommand]
     private async Task OpenProjectManagerWindow()
     {
+        CurrentViewType = ViewType.ProjectManager;
+
         var dialog = ActivatorUtilities.CreateInstance<ProjectSelectionWindow>(serviceProvider);
 
         dialog.DataContext = ActivatorUtilities.CreateInstance<ProjectSelectionViewModel>(serviceProvider, dialog);
 
         await dialog.ShowDialog(window);
+
+        CurrentView = new OverviewView { DataContext = new OverviewViewModel(this) };
+        CurrentViewType = ViewType.Overview;
     }
 
     [RelayCommand]
     private void SetOverviewView()
     {
+        CurrentViewType = ViewType.Overview;
         CurrentView = new OverviewView { DataContext = new OverviewViewModel(this) };
     }
 
     [RelayCommand]
     private void SetDataExportView()
     {
+        CurrentViewType = ViewType.DataExport;
         CurrentView = new DataExportView { DataContext = new DataExportViewModel(assetManager, optimizer, projectManager) };
     }
     
